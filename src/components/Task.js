61f7f87571ff8bccb34ac41c60e155ed61312def
jshifOnix <joshua@onixnet.com>
2021-01-21 14:02:35 -0500

    change task background to red

--- src/components/Task.js
+++ src/components/Task.js
@@ -15,7 +15,13 @@ export default function Task({task: { id, title, state }, onArchiveTask, onPinTa
                <span className="checkbox-custom" onClick={() => onArchiveTask(id)}></span>
            </label>
            <div className="title">
-              <input type="text" value={title} readOnly={true} placeholder="Input title" />
+              <input 
+	         type="text" 
+	         value={title} 
+	         readOnly={true} 
+	         placeholder="Input title"
+	         style={{ background: 'red' }}
+	      />
             </div>
             <div className="actions" onClick={event => event.stopPropagation()}>
                 {state !== 'TASK_ARCHIVED' && (
