650003a8896d4127bb10008548c24bb193079390
jshifOnix <joshua@onixnet.com>
2021-01-21 11:36:57 -0500

    setup store from redux to provide data

--- src/components/Task.js
+++ src/components/Task.js
@@ -17,9 +17,9 @@ export default function Task({task: { id, title, state }, onArchiveTask, onPinTa
            <div className="title">
               <input type="text" value={title} readOnly={true} placeholder="Input title" />
             </div>
-            
-            <div className="actions" onClick={event => event.stopPropagation}>
+            <div className="actions" onClick={event => event.stopPropagation()}>
                 {state !== 'TASK_ARCHIVED' && (
+                    // eslint-disable-next-line jsx-a11y/anchor-is-valid
                    <a onClick={() => onPinTask(id)}>
                      <span className={`icon-star`} />
                    </a>
